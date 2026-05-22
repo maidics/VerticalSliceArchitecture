c5405b1f1a2fd4a38ce3370541890b66e027f532
Marics <your-username@users.noreply.github.com>
2026-05-22 16:50:03 +0200

    Implemented ExceptionHandler

--- src/VsaTemplate/Common/Pipeline/ExceptionHandler.cs
+++ src/VsaTemplate/Common/Pipeline/ExceptionHandler.cs
@@ -1,15 +1,42 @@
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace VsaTemplate.Common.Pipeline;
 
-public class ExceptionHandler : IExceptionHandler
+public sealed class ExceptionHandler : IExceptionHandler
 {
-    public ValueTask<bool> TryHandleAsync(
+    private readonly ILogger<ExceptionHandler> _logger;
+
+    public ExceptionHandler(ILogger<ExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext,
         Exception exception,
         CancellationToken cancellationToken
     )
     {
-        throw new NotImplementedException();
+        _logger.LogError(
+            exception,
+            "Unhandled exception caught while processing request at [{HttpMethod}] {Path}",
+            httpContext.Request.Method,
+            httpContext.Request.Path
+        );
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "https://datatracker.ietf.org/doc/html/rfc9110#name-500-internal-server-error",
+            Title = "Internal Server Error",
+            Detail = "An unexpected error occured.",
+            Instance = httpContext.Request.Path,
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
     }
 }
